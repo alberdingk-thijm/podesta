3d3590c5995f6d9fa080e511be253471c83d7eb3
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2017-03-22 13:22:29 -0400

    Move buildings structs from src/main.rs to new src/buildings.rs

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,6 @@ mod events;
 use std::env;
 //use std::io;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Building {
-    name: String,
-    id: i32,
-    btype: BType,  // use serde macro to match type
-    preq: Option<Vec<String>>,  // can be null
-    cost: f32,
-    build: f32,
-    events: Vec<EventChance>,
-    flags: i32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum BType {
-    Residential,
-    Industrial,
-    Port,
-    Academic,
-    Administrative,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct EventChance {
-    name: String,
-    chance: f32,
-}
-
-
 fn main() {
 
     let events : Vec<events::Event> = parser::get_data("events.json")
